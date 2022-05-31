5240b3c992376febee55e6b051fac532959c1483
medsonmoombe <medsonmoombe21@gmail.com>
2022-05-31 15:30:27 +0200

    Add the component folder and add the calcualater.js file with a function to render the ui

---
+++ src/components/Calculator.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+class Calcu extends React.PureComponent {
+  render() {
+    return (
+      <div className="calculatorDisplay">
+
+        <div className="result">0</div>
+        <div className="operand gry">AC</div>
+        <div className="operand gry">&#43;/&minus;</div>
+        <div className="operand gry">%</div>
+        <div className="operand op">&divide;</div>
+        <div className="digits gry">7</div>
+        <div className="digits gry">8</div>
+        <div className="digits gry">9</div>
+        <div className="digits op">&times;</div>
+        <div className="digits gry">4</div>
+        <div className="digits gry">5</div>
+        <div className="digits gry">6</div>
+        <div className="digits op">-</div>
+        <div className="digits gry">1</div>
+        <div className="digits gry">2</div>
+        <div className="digits gry">3</div>
+        <div className="digits op">+</div>
+        <div className="digits span gry">0</div>
+        <div className="digits gry">.</div>
+        <div className="digits op">=</div>
+      </div>
+    );
+  }
+}
+
+export default Calcu;
