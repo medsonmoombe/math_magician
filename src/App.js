da1f7113c3bdc1770938d3051a359b40d0f0e50e
medsonmoombe <medsonmoombe21@gmail.com>
2022-06-08 16:39:32 +0200

    Add the navbar, home, quotes and modify the calculator components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom';
 import Calcu from './components/Calculator';
+
+import Home from './components/Home';
+import Quote from './components/Quotes';
 
 class App extends React.PureComponent {
   render() {
     return (
-      <div className="display">
-        <Calcu />
-      </div>
+      <>
+
+        <div className="display">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="calculator" element={<Calcu />} />
+            <Route path="Quotes" element={<Quote />} />
+          </Routes>
+
+        </div>
+      </>
     );
   }
 }
---
+++ src/components/Home.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+class Home extends React.PureComponent {
+  render() {
+    return (
+      <div className="home-content">
+        <h2 className="welcome">Welcome to our Page!</h2>
+        <div className="para">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Nemo itaque dignissimos ab corrupti
+            labore ipsum fugit odit quod. Tenetur, deserunt. Hic,
+            minus nulla dignissimos aliquid soluta
+            quos fugiat optio expedita.
+            Consequatur maxime mollitia ipsum culpa rerum quod modi
+            eligendi officia. Nam iste dolorem
+            cumque et ea autem ratione, adipisci sint provident
+            omnis at quos, libero laboriosam maiores.
+            Ratione, fugit possimus?
+            Iste sit repellat, id enim commodi nemo reprehenderit
+            porro architecto soluta quia non ad amet
+            consequatur cum provident, quam, earum quasi?
+            Reprehenderit quaerat delectus repellendus labore.
+            Sit obcaecati eum similique.
+          </p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Nemo itaque dignissimos ab corrupti
+          labore ipsum fugit odit quod. Tenetur, deserunt. Hic,
+          minus nulla dignissimos aliquid soluta
+          quos fugiat optio expedita.
+          Consequatur maxime mollitia ipsum culpa rerum quod modi
+          eligendi officia. Nam iste dolorem
+          cumque et ea autem ratione, adipisci sint provident
+          omnis at quos, libero laboriosam maiores.
+          Ratione, fugit possimus?
+          Iste sit repellat, id enim commodi nemo reprehenderit
+          porro architecto soluta quia non ad amet
+          consequatur cum provident, quam, earum quasi?
+          Reprehenderit quaerat delectus repellendus labore.
+          Sit obcaecati eum similique.
+          <p />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
