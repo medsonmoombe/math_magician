c63f7b5c41df020b737483f173ec4f1a8d64db1b
medsonmoombe <medsonmoombe21@gmail.com>
2022-06-08 17:16:10 +0200

    Modify the calculator  component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,51 +12,43 @@ const Calcu = () => {
     const oprObject = calculate(state, e.target.textContent);
     setState(oprObject);
   };
-  
-    render() {
-      const handleEvent = (e) => {
-        const oprObject = calculate(this.state, e.target.textContent);
-        this.setState(oprObject);
-      };
 
-      const { total, operation, next } = this.state;
-      const oprnd = operation === '%' ? 'mod' : operation;
-      let result = '';
-      if (total) {
-        result = `${total} ${oprnd || ''} ${next || ''}`;
-      } else if (next) {
-        result = `${next} ${oprnd || ''}`;
-      }
-      return (
-        <div className="calculator-dis">
-          <p className="math-intro">Lets do some Math!</p>
-          <div className="calculatorDisplay">
+  const { total, operation, next } = state;
+  const oprnd = operation === '%' ? 'mod' : operation;
+  let result = '';
+  if (total) {
+    result = `${total} ${oprnd || ''} ${next || ''}`;
+  } else if (next) {
+    result = `${next} ${oprnd || ''}`;
+  }
+  return (
+    <div className="calculator-dis">
+      <p className="math-intro">Lets do some Math!</p>
+      <div className="calculatorDisplay">
 
-            <div className="result">{result || 0 }</div>
-            <Keys styles="operand gry" handleEvent={(e) => handleEvent(e)} val="AC" />
-            <Keys styles="operand gry" handleEvent={(e) => handleEvent(e)} val="+/-" />
-            <Keys styles="operand gry" handleEvent={(e) => handleEvent(e)} val="%" />
-            <Keys styles="operand op" handleEvent={(e) => handleEvent(e)} val="÷" />
-            <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="7" />
-            <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="8" />
-            <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="9" />
-            <Keys styles="digits op" handleEvent={(e) => handleEvent(e)} val="x" />
-            <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="4" />
-            <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="5" />
-            <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="6" />
-            <Keys styles="digits op" handleEvent={(e) => handleEvent(e)} val="-" />
-            <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="1" />
-            <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="2" />
-            <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="3" />
-            <Keys styles="digits op" handleEvent={(e) => handleEvent(e)} val="+" />
-            <Keys styles="digits span gry" handleEvent={(e) => handleEvent(e)} val="0" />
-            <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="." />
-            <Keys styles="digits op" handleEvent={(e) => handleEvent(e)} val="=" />
-          </div>
-        </div>
-      );
-    }
-}
-
+        <div className="result">{result || 0 }</div>
+        <Keys styles="operand gry" handleEvent={(e) => handleEvent(e)} val="AC" />
+        <Keys styles="operand gry" handleEvent={(e) => handleEvent(e)} val="+/-" />
+        <Keys styles="operand gry" handleEvent={(e) => handleEvent(e)} val="%" />
+        <Keys styles="operand op" handleEvent={(e) => handleEvent(e)} val="÷" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="7" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="8" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="9" />
+        <Keys styles="digits op" handleEvent={(e) => handleEvent(e)} val="x" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="4" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="5" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="6" />
+        <Keys styles="digits op" handleEvent={(e) => handleEvent(e)} val="-" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="1" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="2" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="3" />
+        <Keys styles="digits op" handleEvent={(e) => handleEvent(e)} val="+" />
+        <Keys styles="digits span gry" handleEvent={(e) => handleEvent(e)} val="0" />
+        <Keys styles="digits gry" handleEvent={(e) => handleEvent(e)} val="." />
+        <Keys styles="digits op" handleEvent={(e) => handleEvent(e)} val="=" />
+      </div>
+    </div>
+  );
+};
 
 export default Calcu;
